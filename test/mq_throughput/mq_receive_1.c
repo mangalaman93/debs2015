bb3f601a0db459ce05d5f2b33ead1577aa55d545
rohandas <rohands22@gmail.com>
2015-01-26 21:47:37 +0530

    data file added with 10005 entries

--- test/mq_throughput/mq_receive_1.c
+++ test/mq_throughput/mq_receive_1.c
@@ -1,25 +1,3 @@
-/*
- *  This program explains POSIX Message Queue APIs
- *
- *  http://www.linuxpedia.org/index.php?title=Linux_System_Programming_:_POSIX_Message_Queue
- *
- *  Copyright (C) 2012  LinuxPedia.org (anoojgopi@linuxpedia.org)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
--- test/mq_throughput/mq_send.c
+++ test/mq_throughput/mq_send.c
@@ -1,33 +1,11 @@
-/*
- *  This program explains POSIX Message Queue APIs
- *
- *  http://www.linuxpedia.org/index.php?title=Linux_System_Programming_:_POSIX_Message_Queue
- *
- *  Copyright (C) 2012  LinuxPedia.org (anoojgopi@linuxpedia.org)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
- *
- */
-
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
- #include <time.h>
+#include <time.h>
 
 #define MQ_NAME1 "/mqconsumer1"
- #define MQ_NAME2 "/mqconsumer2"
+#define MQ_NAME2 "/mqconsumer2"
 #define MQ_MESSAGE_DATA "hello world"
 #define MQ_MESSAGE_LENGTH 100
 #define MQ_MESSAGE_PRIORITY 0
