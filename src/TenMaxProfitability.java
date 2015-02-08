dda2fba4079f469f385544debee86e4699550dc7
Aman Mangal <mangalaman93@gmail.com>
2015-02-08 18:37:17 -0500

    added more code in TenMaxProfitability, working on testing

--- src/TenMaxProfitability.java
+++ src/TenMaxProfitability.java
@@ -10,15 +10,20 @@ class Profitability implements Comparable<Profitability> {
   public Profitability() {
     profit = new Mc();
     num_empty_taxis = 0;
+    ts = null;
+  }
+
+  public float getProfitability() {
+    return this.profit.getMedian()/this.num_empty_taxis;
   }
 
   @Override
   public int compareTo(Profitability ptb) {
-    float f1 = this.profit.getMedian()/this.num_empty_taxis;
-    float f2 = ptb.profit.getMedian()/ptb.num_empty_taxis;
-    if(f1 < f2) {
+    float p1 = this.getProfitability();
+    float p2 = ptb.getProfitability();
+    if(p1 < p2) {
       return -1;
-    } else if(f1 > f2) {
+    } else if(p1 > p2) {
       return 1;
     } else if(this.ts.compareTo(ptb.ts) < 0) {
       return -1;
@@ -33,36 +38,48 @@ class Profitability implements Comparable<Profitability> {
 class ArrayMap extends AbstractMap<Area, Profitability> {
   private int xSize;
   private int ySize;
-  private double[][] data;
+  private Profitability[][] data;
   int size;
 
   public ArrayMap(int xLimit, int yLimit) {
     this.xSize = xLimit;
     this.ySize = yLimit;
-    data = new double[xSize][ySize];
+    data = new Profitability[xSize][ySize];
     size = 0;
 
     for(int i=0; i<xSize; i++)
       for(int j=0; j<ySize; j++)
-        data[i][j] = 0;
+        data[i][j] = null;
   }
 
   public boolean containsKey(Area a) {
-    return (data[a.x][a.y] != 0);
+    return (data[a.x][a.y] != null);
   }
 
   @Override
-  public Profitability put(Area a, Profitability t) {
-    return null;
+  public Profitability put(Area a, Profitability p) {
+    Profitability return_value;
+    if(data[a.x][a.y] != null) {
+      return_value = data[a.x][a.y];
+    } else {
+      this.size++;
+      return_value = null;
+    }
+
+    data[a.x][a.y] = p;
+    return return_value;
   }
 
   public Profitability remove(Area a) {
-    return null;
+    Profitability return_value = data[a.x][a.y];
+    data[a.x][a.y] = null;
+    this.size--;
+    return return_value;
   }
 
   @Override
   public int size() {
-    return 0;
+    return this.size;
   }
 
   @Override
@@ -81,11 +98,15 @@ public class TenMaxProfitability extends TenMax<Area, Profitability> {
 
   @Override
   public Profitability addTwoVals(Profitability v1, Profitability v2) {
-    return null;
+    Profitability p = new Profitability();
+    p.num_empty_taxis = v1.num_empty_taxis + v2.num_empty_taxis;
+    p.ts = v2.ts;
+    p.profit = v1.profit;
+    return p;
   }
 
   @Override
   public boolean isZeroVal(Profitability v) {
-    return false;
+    return v.ts==null;
   }
 }
---
+++ test/TenMaxProfitabilityTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TenMaxProfitabilityTest {
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testTenMaxProfitability1() {
+    fail("Not yet implemented");
+  }
+}
