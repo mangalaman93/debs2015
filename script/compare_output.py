5c65a12ec2b8fff7558ae866b91fd57eda3e519f
Tanmay <tvrandhavane@gmail.com>
2015-03-10 13:31:17 +0530

    Compare output script

---
+++ script/compare_output.py
@@ -0,0 +1,14 @@
+def compare_out(pout, mout):
+  with open(pout, 'r') as pfd, open(mout, 'r') as mfd:
+    pfile = csv.reader(pfd)
+    mfile = csv.reader(mfd)
+    plen = sum(1 for row in pfile)
+    mlen = sum(1 for row in mfile)
+    if plen != mlen:
+      raise Exception("different number of rows!")
+    for prow,mrow in zip(pfile, mfile):
+      for i in range(len(mrow)):
+        if mrow[i] != prow[i]:
+          raise Exception("following rows didn't match-\n"+mrow+"\n"+prows)
+
+compare_out("q1_out_base.csv", "q1_out_hash.csv")
