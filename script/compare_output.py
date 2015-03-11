7324dc91d1630c0144216bfc5c56d090bf865eee
Tanmay <tvrandhavane@gmail.com>
2015-03-11 09:02:46 +0530

    updated compare script

--- script/compare_output.py
+++ script/compare_output.py
@@ -4,16 +4,26 @@ import csv
 
 def compare_out(pout, mout):
   with open(pout, 'r') as pfd, open(mout, 'r') as mfd:
-    pfile = csv.reader(pfd)
-    mfile = csv.reader(mfd)
-    plen = sum(1 for row in pfile)
-    mlen = sum(1 for row in mfile)
-    print plen, mlen
-    if plen != mlen:
-      raise Exception("different number of rows!")
-    for prow,mrow in zip(pfile, mfile):
-      for i in range(len(mrow)):
-        if mrow[i] != prow[i]:
-          raise Exception("following rows didn't match-\n"+mrow+"\n"+prows)
+    for line1 in pfd:
+      for line2 in mfd:
+        array1 = line1.split(',')
+        array2 = line2.split(',')
+        if(len(array1)!= len(array2)):
+          print line1, "\n", line2, "-----------------"
+        else:
+          for i in range(len(array1)):
+            if array1[i] != array2[i]:
+              print line1, "\n", line2, "-----------------"
+  # with open(pout, 'r') as pfd, open(mout, 'r') as mfd:
+  #   pfile = csv.reader(pfd)
+  #   mfile = csv.reader(mfd)
+  #   plen = sum(1 for row in pfile)
+  #   mlen = sum(1 for row in mfile)
+  #   print plen, mlen
+  #   for prow,mrow in zip(pfile, mfile):
+  #     print 1
+  #     for i in range(len(mrow)):
+  #       if mrow[i] != prow[i]:
+  #         raise Exception("following rows didn't match-\n"+mrow+"\n"+prows)
 
 compare_out("out/q1_out_base.csv", "out/q1_out_hash.csv")
